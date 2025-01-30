0f6035733d5b8a4856e118341c0f22461d65f6f2
Parthhh13 <parth.pahwa22@st.niituniversity.in>
2025-01-30 15:25:54 +0530

    Lab 2

--- 04 Component_Labeling.py
+++ 04 Component_Labeling.py
@@ -1,8 +1,12 @@
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
 # Load the image
-'''image = cv2.imread("lenna.jpg", cv2.IMREAD_GRAYSCALE)
-image=cv2.resize(image,(256,256))'''
+image = cv2.imread("thresholding.jpg", cv2.IMREAD_GRAYSCALE)
+#image=cv2.resize(image,(100,100))
+#image=cv2.GaussianBlur(image,(5,5),0)
+
 
 def component_labeling(binary_image):
     rows, cols = binary_image.shape
@@ -12,7 +16,7 @@ def component_labeling(binary_image):
 
     for row in range(rows):
         for col in range(cols):
-            if binary_image[row][col]==1:
+            if binary_image[row][col]==255:
 
                 top_label=labels[row-1][col] if row>0 else 0
                 left_label=labels[row][col-1] if col>0 else 0
@@ -28,27 +32,58 @@ def component_labeling(binary_image):
                     labels[row][col]=left_label
 
                 elif top_label!=0 and left_label!=0:
-                    labels[row][col]=left_label 
+                    min_label=min(top_label,left_label)
+                    max_label=max(top_label,left_label)
 
-                    if top_label!=left_label:
-                        equivalence_dict[top_label]=left_label
-   
+                    labels[row][col]=min_label 
+
+                    if min_label!=max_label:
+                        equivalence_dict[max_label]=min_label
+    def find_root(label):
+        while label in equivalence_dict:
+            label=equivalence_dict[label]
+        return label
+     
+    for label in equivalence_dict.keys():
+        equivalence_dict[label]=find_root(label)
+
     for row in range(rows):
         for col in range(cols):
             if labels[row][col] > 0:  # If pixel is labeled
-                if labels[row][col] in equivalence_dict:
-                     labels[row][col]=equivalence_dict[labels[row][col]]
+                labels[row][col]=find_root(labels[row][col])
+    
+    distinct=np.unique(labels[labels>0])
 
-    return labels
+    return labels,len(distinct)
 
 
-binary_image=np.array([
-    [1,0,1,0,1],
-    [1,0,1,0,1],
-    [1,1,1,0,0],
-    [0,0,0,0,1]
-])
-print(component_labeling(binary_image))
+
+plt.figure(figsize=(8, 6))
+
+# Original Image
+plt.subplot(1, 3, 1)
+plt.title("Original Image")
+plt.imshow(image, cmap='gray')
+plt.axis('off')
+
+thresh=20
+bin = np.where(image > thresh, 255, 0).astype(np.uint8)
+
+plt.subplot(1, 3, 2)
+plt.title(f"Segmented Image T={thresh}")
+plt.imshow(bin, cmap='gray')
+plt.axis('off')
+
+final,labels=component_labeling(bin)
+
+# Original Image
+plt.subplot(1, 3, 3)
+plt.title(f"Labeled Image , No. of objects ={labels}")
+plt.imshow(final, cmap='nipy_spectral')
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
 
 
 
